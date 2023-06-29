b2ed2a56d4802492d00d2ecc0f84d175c6e2ec1c
Jack Le Hamster <jacklehamster@gmail.com>
2023-06-28 20:32:03 -0700

    Expose ValueOf

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,3 @@
 export type GlUsage = "STATIC_DRAW" | "STREAM_DRAW" | "DYNAMIC_DRAW";
 export type GlType = "BYTE" | "FLOAT" | "SHORT" | "UNSIGNED_BYTE" | "UNSIGNED_SHORT" | "INT" | "UNSIGNED_INT" | "FLOAT";
+export type { ValueOf } from "dok-actions";
