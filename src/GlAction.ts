dc500c4e8639d5138d90e08086934ca6a2ce2686
Jack Le Hamster <jacklehamster@gmail.com>
2023-07-25 13:43:58 -0700

    Change texture ID to number.

--- src/GlAction.ts
+++ src/GlAction.ts
@@ -1,6 +1,6 @@
 import { BooleanResolution, DokAction, NumberResolution, StringResolution, TypedArrayResolution } from "dok-actions";
 import { GlBufferTarget, GlType, GlUsage } from "./types";
-import { ImageId, TextureId, Url } from "./ImageId";
+import { ImageId, Url } from "./ImageId";
 
 export type LocationName = string;
 export type LocationResolution = LocationName | StringResolution | [StringResolution<LocationName>, NumberResolution];
@@ -73,7 +73,7 @@ export interface GlAction extends DokAction {
     activateProgram?: StringResolution;
     loadTexture?: {
       imageId: StringResolution<ImageId>;
-      textureId: TextureId;
+      textureId: NumberResolution<0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31>;
       sourceRect?: [NumberResolution, NumberResolution, NumberResolution, NumberResolution];
       destRect?: [NumberResolution, NumberResolution, NumberResolution, NumberResolution];
     };
